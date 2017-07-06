9e74821eb0083eba1b6e5376c465781804dad8e3
Frank S. Thomas <frank@timepit.eu>
2017-07-06 23:18:57 +0200

    Draw on canvas

--- modules/gui/js/src/main/scala/Hello.scala
+++ modules/gui/js/src/main/scala/Hello.scala
@@ -1,14 +1,20 @@
-import org.scalajs.dom
-import dom.document
+import org.scalajs.dom.{CanvasRenderingContext2D, document}
+import org.scalajs.dom.html.Canvas
 
 object Hello {
   def main(args: Array[String]): Unit = {
-    val p = document.createElement("p")
-    val t = document.createTextNode("Hello, world!")
-    p.appendChild(t)
-    document.body.appendChild(p)
+    val canvas = document.createElement("canvas").asInstanceOf[Canvas]
+    document.body.appendChild(canvas)
 
-    println("Hello, world!")
-    println("EOF")
+    val ctx = canvas.getContext("2d").asInstanceOf[CanvasRenderingContext2D]
+
+    def drawPoint(x: Double, y: Double) =
+      ctx.fillRect(x, y, 5, 5)
+
+    ctx.fillStyle = "red"
+    drawPoint(10, 10)
+    drawPoint(20, 20)
+
+    ()
   }
 }
