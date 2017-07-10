b684eb1cc40a72cd4a752f7be1ab01ea21a1aee1
Frank S. Thomas <frank@timepit.eu>
2017-07-11 00:27:12 +0200

    Consistent use of type aliases

--- modules/core/shared/src/main/scala/kmeans/KMeans2D.scala
+++ modules/core/shared/src/main/scala/kmeans/KMeans2D.scala
@@ -9,12 +9,12 @@ final class KMeans2D extends KMeans {
   override type Point = Point2D
   override type Distance = Double
 
-  override def distance(p1: Point, p2: Point): Double =
-    math.sqrt(math.pow(p1.x - p2.x, 2) + math.pow(p1.y - p2.y, 2))
+  override def distance(p1: Point, p2: Point): Distance =
+    math.sqrt(math.pow(p1.x - p2.x, 2.0) + math.pow(p1.y - p2.y, 2.0))
 
-  override def calculateMean(points: List[Point2D]): Mean = {
+  override def calculateMean(points: List[Point]): Mean = {
     val scale = 1.0 / points.size
-    val sum = points.reduce[Point2D]((p1, p2) => p1.add(p2))
+    val sum = points.reduce[Point]((p1, p2) => p1.add(p2))
     Mean(Point2D(sum.x * scale, sum.y * scale))
   }
 
